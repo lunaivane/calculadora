b2dbccce40d7e01b169ad2231d250ea7e173d454
Lunaivane <lunaivane@gmail.com>
2017-07-05 09:59:10 -0300

    Subida inicial

---
+++ src/negocio/calculo.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocio;
+
+/**
+ *
+ * @author Sello
+ */
+public class calculo {
+    private Double num1, num2, num3;
+    private Double rst; 
+
+    public Double getNum1() {
+        return num1;
+    }
+
+    public void setNum1(Double num1) {
+        this.num1 = num1;
+    }
+
+    public Double getNum2() {
+        return num2;
+    }
+
+    public void setNum2(Double num2) {
+        this.num2 = num2;
+    }
+
+    public Double getNum3() {
+        return num3;
+    }
+
+    public void setNum3(Double num3) {
+        this.num3 = num3;
+    }
+
+    public Double getRst() {
+        return rst;
+    }
+    
+ 
+}
